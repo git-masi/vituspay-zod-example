afd9f514c8f15866a4ee4d057dbf8e06fcd08dee
Eric Masi <emasi@vitusvet.com>
2023-02-06 08:52:33 -0500

    Use dependency injection for repository

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { authRouter } from "./auth/auth";
+import { initDynamoDbRepository } from "./transactions/dynamoDb";
 import { initTransactionsRouter } from "./transactions/transactions";
 
 const app = express();
 const port = 3000;
-const transactionsRouter = initTransactionsRouter();
+const transactionsRouter = initTransactionsRouter(initDynamoDbRepository());
 
 app.use(bodyParser.json());
 
--- src/transactions/transactions.ts
+++ src/transactions/transactions.ts
@@ -1,41 +1,28 @@
-import { Router } from "express";
-import { ZodError } from "zod";
-import { initDynamoDbRepository } from "./dynamoDb";
+import { Router, RequestHandler } from "express";
 import { TransactionType } from "./types";
 
 interface GetTransactionsActions {
   getTransactions: () => Promise<Array<TransactionType>>;
 }
 
-export function initTransactionsRouter() {
-  const transactionsRouter = Router();
-  const dynamodbRepository = initDynamoDbRepository();
+type TransactionsRepository = GetTransactionsActions;
 
-  // =============
-  // Refactor Note
-  // =============
-  // In the future when there are multiple functions that require these
-  // arguments we can create a pipeline to initialize them rather than calling
-  // then one bye one like this
-  getTransactions(transactionsRouter, dynamodbRepository);
+export function initTransactionsRouter(repository: TransactionsRepository) {
+  const transactionsRouter = Router();
+
+  transactionsRouter.get("/", getTransactions(repository));
 
   return transactionsRouter;
 }
 
-function getTransactions(router: Router, actions: GetTransactionsActions) {
-  router.get("/", async (req, res) => {
+function getTransactions(actions: GetTransactionsActions): RequestHandler {
+  return async (req, res) => {
     try {
       res.send(await actions.getTransactions());
     } catch (error) {
-      if (error instanceof ZodError) {
-        console.error(error.issues);
-        res.sendStatus(400);
-        return;
-      }
-
       console.error(error);
 
       res.sendStatus(500);
     }
-  });
+  };
 }
