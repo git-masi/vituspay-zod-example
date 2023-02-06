e5947975c112d494b8d81f4a68137a10c2f24f4d
Eric Masi <emasi@vitusvet.com>
2023-02-05 22:01:28 -0500

    Initialize the transactions router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { authRouter } from "./auth/auth";
+import { initTransactionsRouter } from "./transactions/transactions";
 
 const app = express();
 const port = 3000;
+const transactionsRouter = initTransactionsRouter();
 
 app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
+
+app.use("/transactions", transactionsRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
