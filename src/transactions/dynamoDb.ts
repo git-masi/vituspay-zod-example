0723a8f34bb9b5db5a3e997df7707142f24f683d
Eric Masi <emasi@vitusvet.com>
2023-02-05 22:00:53 -0500

    Add a repository for interacting with DynamoDb

---
+++ src/transactions/dynamoDb.ts
@@ -0,0 +1,30 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { TransactionsType, Transactions } from "./types";
+
+const TableName = "VitusPay-Resources_Transactions_dev";
+
+export interface DynamodbRepository {
+  getTransactions: () => Promise<TransactionsType>;
+}
+
+export function initDynamoDbRepository(): DynamodbRepository {
+  // =============
+  // Refactor Note
+  // =============
+  // The region should be dynamic in the future
+  const dbClient = new DynamoDBClient({ region: "us-east-1" });
+  const documentClient = DynamoDBDocumentClient.from(dbClient);
+
+  return {
+    getTransactions: getTransactions(documentClient),
+  };
+}
+
+function getTransactions(client: DynamoDBDocumentClient) {
+  return async () => {
+    const { Items } = await client.send(new QueryCommand({ TableName }));
+
+    return Transactions.parse(Items);
+  };
+}
