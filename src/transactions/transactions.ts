f920ee09ff0f35677eb3823081789120c55b59f8
Eric Masi <emasi@vitusvet.com>
2023-02-06 09:24:23 -0500

    Add a new example transactions route

--- src/transactions/transactions.ts
+++ src/transactions/transactions.ts
@@ -1,16 +1,34 @@
 import { Router, RequestHandler } from "express";
-import { TransactionType } from "./types";
+import { ZodError } from "zod";
+import {
+  TransactionAmount,
+  TransactionAmountType,
+  TransactionIdType,
+  TransactionType,
+} from "./types";
 
 interface GetTransactionsActions {
   getTransactions: () => Promise<Array<TransactionType>>;
 }
 
-type TransactionsRepository = GetTransactionsActions;
+interface CreateTransactionActions {
+  createTransaction: (
+    amount: TransactionAmountType
+  ) => Promise<TransactionIdType>;
+}
+
+// The `TransactionsRepository` is a collection of actions required to init all
+// of the route handlers. Actions are just a collection of functionality required
+// to do some useful work like updating a database.
+// It is a sum type like `SomeActions & SomeOtherActions` and can be extended
+// with more types as needed.
+type TransactionsRepository = GetTransactionsActions & CreateTransactionActions;
 
 export function initTransactionsRouter(repository: TransactionsRepository) {
   const transactionsRouter = Router();
 
   transactionsRouter.get("/", getTransactions(repository));
+  transactionsRouter.post("/", createTransaction(repository));
 
   return transactionsRouter;
 }
@@ -21,6 +39,27 @@ function getTransactions(actions: GetTransactionsActions): RequestHandler {
       res.send(await actions.getTransactions());
     } catch (error) {
       console.error(error);
+
+      res.sendStatus(500);
+    }
+  };
+}
+
+function createTransaction(actions: CreateTransactionActions): RequestHandler {
+  return async (req, res) => {
+    try {
+      const amount = TransactionAmount.parse(req.body.amount);
+
+      const id = await actions.createTransaction(amount);
+
+      res.status(201).send({ id });
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof ZodError) {
+        res.sendStatus(400);
+        return;
+      }
 
       res.sendStatus(500);
     }
